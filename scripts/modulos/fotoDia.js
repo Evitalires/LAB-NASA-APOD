94c9e6a3796180674e6a194fe06a238b22e6801d
JefferssonISWilches <jeffersonivan1999@gmail.com>
2025-11-27 10:26:05 -0500

    Actualizo fotoDia.html, index.html y fotoDia.js

--- scripts/modulos/fotoDia.js
+++ scripts/modulos/fotoDia.js
@@ -1,6 +1,29 @@
-/*filtra por imagenes y devuelve un objeto */
+import { getApodByDate } from "./fetchNasa.js";
 
-export const fotoDia = () => {
-    return fetchNasa()
-    /* O devuelve el dato, o lo usa para volver a mostar la foto del dia */
+const img = document.getElementById("apodImagen");
+const titulo = document.getElementById("apodTitulo");
+const fecha = document.getElementById("apodFecha");
+const descripcion = document.getElementById("apodDescripcion");
+const btnFav = document.getElementById("btnFavorito");
+
+// Pinta la card con la data
+export function renderFoto(data) {
+    titulo.textContent = data.title;
+    fecha.textContent = data.date;
+    descripcion.textContent = data.explanation;
+
+    if (data.media_type === "image") {
+        img.src = data.url;
+        img.style.display = "block";
+    } else {
+        img.src = "";
+        img.style.display = "none";
+    }
+}
+
+// Foto del día
+export async function cargarFotoHoy() {
+    const today = new Date().toISOString().split("T")[0];
+    const data = await getApodByDate(today);
+    renderFoto(data);
 }
